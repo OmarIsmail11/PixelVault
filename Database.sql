ad42b1fab01a394cc0dba7b798e38eb1b3733d66
Mark-2005 <markmagednagek@gmail.com>
2024-11-28 21:30:21 +0200

    Sho5l 3aly

--- Database.sql
+++ Database.sql
@@ -27,7 +27,7 @@
 	BeginDate	DATE ,
 	Organizer	varchar(30),
 	PRIMARY KEY(TName) ,
-	FOREIGN KEY (Organizer) REFERENCES GamingStore
+	FOREIGN KEY (Organizer) REFERENCES GamingStore(StoreName)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 	);
@@ -38,7 +38,7 @@
     ReviewerName varchar(30),
     ReviewerURL text NOT NULL,
     LaunchDate date,
-    PRIMARY KEY (Name),
+    PRIMARY KEY (ReviewerName),
 
 	);
 
@@ -48,8 +48,13 @@
 	AwardName CHAR(20) NOT NULL,
 	Genre CHAR(20) NOT NULL,
 	Reward CHAR(20) ,
-	FOREIGN KEY (Reward) REFERENCES GameReviewer
+	Won	CHAR(20) ,
+	YearWon	INT ,
+	FOREIGN KEY (Reward) REFERENCES GameReviewer(ReviewerName)
 		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY (Won) REFERENCES Game(GameName)
+		ON DELETE CASCADE 
 		ON UPDATE CASCADE
 
 	);
@@ -60,7 +65,7 @@
 	ConsoleName	CHAR(20) ,
 	ReleaseDate	DATE NOT NULL,
 	Develop CHAR(30) ,
-	FOREIGN KEY (Develop) REFERENCES Manufacturer
+	FOREIGN KEY (Develop) REFERENCES Manufacturer (ManufacturerName)
 		ON DELETE SET NULL
 		ON UPDATE CASCADE
 	);
@@ -93,4 +98,78 @@
 	Country		CHAR(20)	NOT NULL,
 	BirthDate	DATE,
 
+	PRIMARY KEY(GamerID)
+	);
+
+	GO
+
+	CREATE TABLE Game(
+    GameName varchar(50),
+    Genre varchar(30) not null check (Genre IN ('Sports', 'Racing', 'Fighting', 'Battle Royale', 'FPS')),
+    ReleaseDate date,
+    ConsoleName char(20),
+    EngineName char(20),
+    Publisher char(20),
+    Rating int default 0,
+    Reviewer varchar(30),
+    primary key (GameName),
+    foreign key (ConsoleName) references Console(ConsoleName)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL,
+    foreign key (EngineName) references GameEngine(EngineName)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL,
+    foreign key (Publisher) references GamePublisher(PublisherName)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL,
+    foreign key (Reviewer) references GameReviewer(ReviewerName)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL,
+	);
+
+	GO
+
+	CREATE TABLE Inventory(
+	GameName	CHAR(20),
+	StoreName varchar(30),
+	Price	Decimal(3,2) NOT NULL,
+	PRIMARY KEY(GameName,StoreName),
+	FOREIGN KEY (Gamename) REFERENCES Game(GameName)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY (StoreName) REFERENCES GamingStore(StoreName)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+	);
+
+	GO
+
+	CREATE TABLE Plays(
+	GameName	CHAR(20),
+	GamerID		INT,
+	CompletionStatus		CHAR(3) CHECK(CompletionStatus IN ('Yes','No')),
+	Difficulty	CHAR(6) CHECK(Difficulty IN('Hard' , 'Medium' , 'Easy')) NOT NULL,
+	UserRating INT CHECK(UserRating Between 1 AND 5),
+	PRIMARY KEY(GameName,GamerID),
+	FOREIGN KEY (Gamename) REFERENCES Game(GameName)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY (GamerID) REFERENCES Gamer(GamerID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+	);
+
+	GO
+
+	CREATE TABLE Enroll(
+	GamerID INT,
+	TName CHAR(20),
+	Ranking INT DEFAULT 0,
+	PRIMARY KEY(GamerID,TName),
+	FOREIGN KEY (GamerID) REFERENCES Gamer(GamerID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY(TName) REFERENCES Tournament(TName)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 	);
