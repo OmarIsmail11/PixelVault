5bcdfda36b8ef12d073b1ce428dd57236b9d6065
TarekOsama528 <67550435+TarekOsama528@users.noreply.github.com>
2024-11-28 18:52:44 +0200

    S

--- PixelVaultSQL/PixelVaultSQL/ApplicationRole1.sql
+++ PixelVaultSQL/PixelVaultSQL/ApplicationRole1.sql
@@ -1,10 +1,19 @@
-﻿CREATE APPLICATION ROLE [ApplicationRole1]
-	WITH PASSWORD = 'xjr{m.zRULisofsqnRTht|crmsFT7_&#$!~<fqqjfeau%iy;'
+﻿/*CREATE APPLICATION ROLE [ApplicationRole1]
+	WITH PASSWORD = 'xjr{m.zRULisofsqnRTht|crmsFT7_&#$!~<fqqjfeau%iy;'*/
 
 
 	CREATE TABLE GameEngine(
-	EngineName CAHR(20) ,
+	EngineName CHAR(20) ,
 	Language VARCHAR(20) NOT NULL ,
 	Platform CHAR(20) NOT NULL ,
 	PRIMARY KEY(EngineName)
+	);
+
+	GO
+
+	CREATE TABLE Manufacturer (
+	ManufacturerName CHAR(30),
+	Country CHAR(20) NULL,
+	PRIMARY KEY(ManufacturerName)
+
 	);
