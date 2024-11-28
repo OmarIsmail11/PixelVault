f925ed2f841a3ab84ef6c0e80954661e6b2c789f
Mark-2005 <markmagednagek@gmail.com>
2024-11-28 19:31:36 +0200

    Sho5l 3alyy

--- Database.sql
+++ Database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE GameEngine(
+	CREATE TABLE GameEngine(
 	EngineName CHAR(20) ,
 	Language VARCHAR(20) NOT NULL ,
 	Platform CHAR(20) NOT NULL ,
@@ -11,5 +11,28 @@ CREATE TABLE GameEngine(
 	ManufacturerName CHAR(30),
 	Country CHAR(20) NULL,
 	PRIMARY KEY(ManufacturerName)
+
+	);
+
+	GO
+
+	CREATE TABLE Tournament(
+	TName CHAR(20),
+	Capacity INT NOT NULL,
+	PrizeMoney DECIMAL(7,2) ,
+	TournamentType	VARCHAR(20) NOT NULL CHECK (TournamentType IN ('Sports' , 'Racing' , 'Fighting', 'Battle Royale' , 'FPS' ) ) ,
+	TLocation	CHAR(20) NOT NULL,
+	BeginDate	DATE ,
+	PRIMARY KEY(TName)
+
+	);
+
+	GO
+
+	CREATE TABLE GameReviewer(
+    ReviewerName varchar(30),
+    ReviewerURL text NOT NULL,
+    LaunchDate date,
+    PRIMARY KEY (Name),
 
 	);
