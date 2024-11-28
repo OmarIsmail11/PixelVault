637ef203c950e779eabb3575997d307be5d47c10
Mark-2005 <markmagednagek@gmail.com>
2024-11-28 18:47:39 +0200

    GameEngine

--- PixelVaultSQL/PixelVaultSQL/ApplicationRole1.sql
+++ PixelVaultSQL/PixelVaultSQL/ApplicationRole1.sql
@@ -2,3 +2,9 @@
 	WITH PASSWORD = 'xjr{m.zRULisofsqnRTht|crmsFT7_&#$!~<fqqjfeau%iy;'
 
 
+	CREATE TABLE GameEngine(
+	EngineName CAHR(20) ,
+	Language VARCHAR(20) NOT NULL ,
+	Platform CHAR(20) NOT NULL ,
+	PRIMARY KEY(EngineName)
+	);
