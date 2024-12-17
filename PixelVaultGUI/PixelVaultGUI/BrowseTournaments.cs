0b8ad8dbf09f63e0662e86e9cf370e25bfb8bb6e
Omar Reda <omar.reda13231@gmail.com>
2024-12-17 12:03:56 +0200

    Started to implement the tournaments filtering feature

---
+++ PixelVaultGUI/PixelVaultGUI/BrowseTournaments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelVaultGUI
+{
+    public partial class BrowseTournaments : UserControl
+    {
+        public BrowseTournaments()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void BrowseTournaments_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
